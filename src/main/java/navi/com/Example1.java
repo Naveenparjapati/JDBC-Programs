1cf536819685a5f8e52ec5ff8a8c2c9adaeb6425
Naveen-kkkk <naveenkumarbalsamand123@gmail.com>
2025-06-29 14:54:53 +0530

    create table done

--- src/main/java/navi/com/Example1.java
+++ src/main/java/navi/com/Example1.java
@@ -11,9 +11,8 @@ public class Example1 {
 			   Class.forName("com.mysql.cj.jdbc.Driver");
 			//Establish the Connection with Database**
 			  // Connection c = DriverManager.getConnection("jdbc:mysql://localhost:3306/database_name", "username", "password");
-			   Connection c=DriverManager.getConnection("jdbc:mysql://localhost:3306/", "root", "root");
-			   
-			   CallableStatement p=c.prepareCall("create database fgd");
+			   Connection c=DriverManager.getConnection("jdbc:mysql://localhost:3306/fgd", "root", "root");
+ CallableStatement p=c.prepareCall("CREATE TABLE STUDENT (NAME VARCHAR(100) NOT NULL, MARKS DECIMAL(5,2),ADDRESS TEXT, PHONE_NUMBER VARCHAR(15));");
 			   boolean e = p.execute();
 			//Process the Request
 			   System.out.println(e);
